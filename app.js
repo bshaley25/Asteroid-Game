a9ac7f9f7eedc6de716a8a0b0c14253aece079b9
Bshaley25 <bshaley25@gmail.com>
2020-02-19 15:05:39 -0700

    ship physics is up and running

--- app.js
+++ app.js
@@ -6,9 +6,6 @@ const c = canvas.getContext('2d')
 
 let asteriodImg = new Image();
 asteriodImg.src = './img/animated_asteroid2.png';
-asteriodImg.onload = function() {
-    animate();
-};
 
 let shipImage = new Image()
 shipImage.src = './img/spaceShip.png'
@@ -28,6 +25,7 @@ const shipWidth = 64;
 const shipeHeight = 64;
 const shipScaledWidth =  shipScale * shipWidth;
 const shipScaledHeight = shipScale * shipeHeight;
+const shipMaxDxDy = 5;
 
 
 const controller = {
@@ -48,9 +46,9 @@ for(let i=0;i<astroidNumber;i++) {
     let frameY = Math.floor( Math.random()* 1.99 )
     let spinSpeed = Math.floor( Math.random()* 10 ) + 3
     let frameNumber = Math.floor( Math.random()* 6 )
-
+    
     asteriodArray.push( new Asteriod(x,y,dx,dy,frameX,frameY, spinSpeed, frameNumber) )
-
+    
 }
 
 
@@ -82,6 +80,7 @@ function Asteriod( x, y, dx, dy, frameX, frameY, spinSpeed, frameNumber) {
         
         if(x < (width/2) || x > (canvas.width - (width/2))) {
             dx = -dx
+            
         }
         if(y < (height/2) || y > (canvas.height - (height/2))) {
             dy = -dy
@@ -90,26 +89,33 @@ function Asteriod( x, y, dx, dy, frameX, frameY, spinSpeed, frameNumber) {
         frameNumber++
         x+=dx
         y+=dy
+        
     }
 }
 
 
 function Ship(x,y,dx,dy, frameX, frameY) {
-
+    
+    
     this.x = x
     this.y = y
     this.dx = dx 
     this.dy = dy
     this.frameX = frameX
     this.frameY = frameY
-
+    
     this.draw = function() {
         c.drawImage(shipImage, shipWidth * frameX, shipWidth * frameY, shipWidth, shipeHeight, (x - shipWidth/2), (y - shipeHeight/2), shipScaledWidth, shipScaledHeight);
     }
-
+    
     this.update = function () {
-
-
+        
+        let theta = (((5 * frameX) + (30 * frameY)) * Math.PI/180)
+        
+        x+=dx
+        y+=dy
+        
+        
         if (controller.button === "ArrowRight") {
             frameX++
             if (frameX == 6) {
@@ -120,10 +126,10 @@ function Ship(x,y,dx,dy, frameX, frameY) {
                 }
             }
         }
-
+        
         if (controller.button === "ArrowLeft") {
             frameX--
-            if (frameX == 0) {
+            if (frameX ==  -1) {
                 frameX = 5
                 frameY--   //go to previous line once the first sprite sheet row has been exhuasted
                 if(frameY == -1){
@@ -131,28 +137,74 @@ function Ship(x,y,dx,dy, frameX, frameY) {
                 }
             }
         }
+        
+        if(controller.button === "ArrowUp") {
+            console.log(theta, Math.sin(theta), Math.cos(theta), dx, dy)
+            dx += Math.sin(theta) * .2
+            dy -= Math.cos(theta) * .2
 
+            if(Math.sign(dx) === 1 && dx > 5) {
+                dx = 5
+            } else if (Math.sign(dx) === -1 && dx < -5) {
+                dx = -5
+            }
 
+            if(Math.sign(dy) === 1 && dy > 5) {
+                dy = 5
+            } else if (Math.sign(dy) === -1 && dy < -5) {
+                dy = -5
+            }
 
+        }
+        
+        if(controller.button === "ArrowDown") {
+
+            if(Math.sign(dx) === 1) {
+                dx -= .5
+            } else if (Math.sign(dx) === -1) {
+                dx += .5
+            }
+
+            if(Math.sign(dy) === 1) {
+                dy *= .5
+            } else if (Math.sign(dy) === -1) {
+                dy *= .5
+            }
+
+        }
+        
+        
+        if (x > window.innerWidth ) {
+            x = 0
+        } else if (x < 0) {
+            x = window.innerWidth - 200
+        }
+        
+        if (y > window.innerHeight) {
+            y = 0
+        } else if (y < 0) {
+            y = window.innerHeight - 100
+        }
+        
     }
-
 }
+
 
 
 function clearCanvas() {
     c.clearRect(0,0,canvas.width, canvas.height)
 }
 
-function asteriodAnimate() {
-
+function gameAnimate() {
+    
     asteriodArray.forEach(asteriod => {
         asteriod.draw()
         asteriod.update()
     });
-
+    
     s1.draw()
     s1.update()
-
+    
 }
 
 function clearController() {
@@ -162,14 +214,18 @@ function clearController() {
 const s1 = new Ship(100,100,0,0,0,0)
 
 function animate() {
-
-
+    
+    
     clearCanvas()
-
-    asteriodAnimate()
+    
+    gameAnimate()
     clearController()
-
+    
     requestAnimationFrame(animate)
 }
+
+document.body.onload = function() {
+    animate();
+};
 
 
